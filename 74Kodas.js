80361a70e0bf7febf919ef1234032593c89ba980
AliusGrigaliunas <Alius.grigaliunas@gmail.com>
2022-07-11 15:53:50 +0300

    Work done 11-7

--- 74Kodas.js
+++ 74Kodas.js
@@ -6,14 +6,20 @@ let loadBox = document.querySelector('.loading');
 loading.textContent = 'Loading...'
 
 loadBox.append(loading);
+let posts_html_link = document.createElement('a');
+posts_html_link.href = 'posts.html?variable=0';
+posts_html_link.textContent = 'Kiti autoriaus įrašai'
 
-fetch('https://jsonplaceholder.typicode.com/posts?_limit=15').then(res=>res.json()).then(data=>{
-    data.map((post,index)=>{
+fetch('https://jsonplaceholder.typicode.com/posts?_limit=5').then(res=>res.json()).then(data=>{
+    data.map((post)=>{
         let div = document.createElement('div');
         div.setAttribute('style','border:1px solid black');
 
         let post_title = document.createElement('h1');
-        post_title.textContent = post.title
+        let a = document.createElement('a');
+        a.textContent =  post.title;
+        a.href = `post.html?userId=${post.id}`
+        post_title.append(a);
 
         let post_description = document.createElement('p');
         post_description.textContent = post.body
@@ -21,6 +27,7 @@ fetch('https://jsonplaceholder.typicode.com/posts?_limit=15').then(res=>res.json
         let comment_item = document.createElement('div')
 
         let post_link = document.createElement('a');
+        post_link.href = `post.html?userId=${post.userId}`
 
         let button = document.createElement('button')
 
@@ -56,14 +63,44 @@ fetch('https://jsonplaceholder.typicode.com/posts?_limit=15').then(res=>res.json
                 button.textContent = 'Show hell';
             }
         })
-        div.append(post_title,post_description,post_link,button,comment_item)
+        div.append(post_title,post_description,post_link,button,comment_item,posts_html_link)
         container.append(div);
     })
     loading.style.display = 'none'
 })
 
-function loader(){
-    container.style.display = 'block'
+function albumList(){
+    let albumContainer = document.querySelector('.container_Photos')
+
+    let NameContainer = document.querySelector('.albums-name');
+    let albumName = document.createElement('a');
+
+    let albumAuthor = document.querySelector('.albums-author');
+    let albumPhoto = document.querySelector('.albums-pic');
+
+    let title = document.createElement('h2');
+    title.innerHTML = `AlbumList:`
+    albumContainer.prepend(title)
+
+    fetch('https://jsonplaceholder.typicode.com/albums?_limit=1').then(res => res.json()).then(albums=>{
+        albums.map(album=>{
+            albumName.textContent = 'Title:' + ' ' + album.title;
+            albumName.href = 'album.html'
+
+                fetch(`https://jsonplaceholder.typicode.com/users/${album.userId}`).then(res=>res.json()).then(userName=>{
+                    albumAuthor.textContent = 'Albums author:' + ' ' + userName.name;
+
+                        fetch(`https://jsonplaceholder.typicode.com/albums/${album.id}/photos?_limit=1`).then(res=>res.json()).then(photos=>{
+                                albumPhoto.innerHTML = `<img src="${photos[0].thumbnailUrl}"></img>`
+                })
+            })
+        })
+        NameContainer.append(albumName)
+    })
 }
+
+
+albumList()
+
 
 
---
+++ search.js
@@ -0,0 +1,74 @@
+const urlParam = document.location.search
+const param = new URLSearchParams(urlParam);
+const results = param.get('search-input')
+
+let text = document.querySelector('.answer');
+
+let text_posts = document.createElement('div')
+let profileText = document.createElement('div');
+
+document.body.append(profileText,text_posts);
+
+profileText.innerHTML = `<h1>Users:</h1>`
+text_posts.innerHTML = `<h1>Posts:</h1>`
+
+
+if(results == ''){}
+else{
+    fetch(`https://jsonplaceholder.typicode.com/users?username_like=${results}`).then(res=>res.json()).then(username=>{
+    console.log(username);
+    if(username[0]){
+        profile(username)
+    }else{
+        fetch(`https://jsonplaceholder.typicode.com/users?name_like=${results}`).then(res=>res.json()).then(Name=>{
+            console.log(Name);
+            if(Name[0]){
+                profile(Name)
+            }
+            else{
+                fetch(`https://jsonplaceholder.typicode.com/users?email_like=${results}`).then(res=>res.json()).then(email=>{
+                    console.log(email);
+                    if(email[0]){
+                        profile(email)
+                    }else{
+                        text.innerHTML = `<div>Nerasta</div>`
+                    }
+                })
+            }
+        })
+    }
+})
+
+    fetch(`https://jsonplaceholder.typicode.com/posts?title_like=${results}`).then(res=>res.json()).then(titles=>{
+        titles.map(post=>{
+            let li = document.createElement('li');
+            li.innerHTML = `<a href=post.html?userId=${post.id}>${post.title}</a>`;
+            text_posts.append(li);
+        })
+})
+}
+
+
+function profile(info){
+    info.map(singleInfo=>{
+        let li = document.createElement('li');
+        li.innerHTML = `<a href=user.html?userId=${singleInfo.id}>${singleInfo.name}</a>`;
+        profileText.append(li)
+//         name.innerHTML = `<div class="container">
+//         <div class="user-wrapper">
+//             <p>Name: <strong>${singleInfo.name}</strong></p>
+//             <p>Username: (<strong>${singleInfo.username}</strong>)</p>
+//             <p><strong>City:</strong>${singleInfo.address.city}</p>
+//             <p><strong>Street:</strong>${singleInfo.address.street}</p>
+//             <p><strong>House number:</strong>${singleInfo.address.suite}</p>
+//             <p><strong>Zipcode:</strong>${singleInfo.address.zipcode}</p>
+//             <p><a href="callto:${singleInfo.phone}"><strong>Number:${singleInfo.phone}</strong></a>
+//             <p><a href=https://www.google.com/maps/@${singleInfo.address.geo.lat},${singleInfo.address.geo.lng},14z><strong>Location</storng></a></p>
+//             <p><strong>Website: </strong>${singleInfo.website}</p>
+//             <p><strong>Company: </strong>${singleInfo.company.name}<p>
+//             <p><strong>${singleInfo.company.bs}</strong></p>
+//         </div>
+//     </div>
+// `                       
+    })                
+}
