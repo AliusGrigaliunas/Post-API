19e2ea57a2db77e42a4075b742a9ea7627bb5a7d
AliusGrigaliunas <Alius.grigaliunas@gmail.com>
2022-07-13 13:05:13 +0300

    users update

--- users.js
+++ users.js
@@ -1,8 +1,12 @@
-let container = document.querySelector('.container');
-let userBox = document.createElement('div');
-fetch('https://jsonplaceholder.typicode.com/users?_embed=posts').then(res=>res.json()).then(users=>{
-    users.map(user=>{
-        userBox.innerHTML +=`<h1><a href='./user.html?userId=${user.id}'>${user.name} (${user.username})</a> ${user.posts.length}</h1>`
-        container.append(userBox)
+function init(){
+    let container = document.querySelector('.container');
+    let userBox = document.createElement('div');
+    fetch('https://jsonplaceholder.typicode.com/users?_embed=posts').then(res=>res.json()).then(users=>{
+        users.map(user=>{
+            userBox.innerHTML +=`<h1><a href='./user.html?userId=${user.id}'>${user.name} (${user.username})</a> ${user.posts.length}</h1>`
+            container.append(userBox)
+        })
     })
-})
+}
+
+init();
